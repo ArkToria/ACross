5b1cae1d0674b9f1fadb3c546cd51b95671ff116
cocomeow <catnights@pm.me>
2021-09-09 17:23:21 +0800

    fix application display name

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ main(int argc, char* argv[])
   QApplication::setOrganizationName("arktoria");
   QApplication::setApplicationName(APP_NAME);
   QApplication::setOrganizationDomain("arktoria.org");
-  QApplication::setApplicationDisplayName("V2ray Client");
+  QApplication::setApplicationDisplayName("V2Ray Client");
   QApplication::setWindowIcon(QIcon::fromTheme("org.arktoria.across"));
   QApplication::setFont(QFont("Noto Sans", 10));
 
