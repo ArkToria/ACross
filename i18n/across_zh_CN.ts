f433d9d6f660ee2437522d39b6bb19b900f07fdb
cocomeow <catnights@pm.me>
2021-09-18 17:23:14 +0800

    rebase from master branch

--- i18n/across_zh_CN.ts
+++ i18n/across_zh_CN.ts
@@ -504,17 +504,17 @@
         <translation>创建新配置</translation>
     </message>
     <message>
-        <location filename="../src/views/home/NodeForm.qml" line="95"/>
+        <location filename="../src/views/home/NodeForm.qml" line="88"/>
         <source>Outbound</source>
         <translation>出站</translation>
     </message>
     <message>
-        <location filename="../src/views/home/NodeForm.qml" line="175"/>
+        <location filename="../src/views/home/NodeForm.qml" line="168"/>
         <source>Accept</source>
         <translation>接受</translation>
     </message>
     <message>
-        <location filename="../src/views/home/NodeForm.qml" line="185"/>
+        <location filename="../src/views/home/NodeForm.qml" line="178"/>
         <source>Cancel</source>
         <translation>取消</translation>
     </message>
