b09c5fdc529db7705f1e9b8d18e247d53ba2c08b
cocomeow <catnights@pm.me>
2021-10-13 23:49:04 +0800

    rebase from master

--- cmake/buildinfo.cmake
+++ cmake/buildinfo.cmake
@@ -1,6 +1,8 @@
 file(STRINGS "${CMAKE_SOURCE_DIR}/misc/VERSION" GUI_VERSION)
 
-set(BUILD_INFO "Manually Build")
+set(BUILD_INFO "Manually Build" CACHE STRING "Display BuildInfo")
+
+set(FONTS_FALLBACK "Noto Sans, Segoe UI Variable, Segoe UI, Noto Sans CJK, PingFang SC, Microsoft YaHei UI" CACHE STRING "Fonts Fallback")
 
 configure_file(${CMAKE_SOURCE_DIR}/src/view_models/buildinfo.h.in
     ${CMAKE_BINARY_DIR}/generated/buildinfo.h
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
 #include "app.h"
+#include "buildinfo.h"
 
 int
 main(int argc, char* argv[])
 {
+  auto fonts = across::getFonts();
   QApplication::setOrganizationName("arktoria");
   QApplication::setApplicationName(APP_NAME);
   QApplication::setOrganizationDomain("arktoria.org");
   QApplication::setApplicationDisplayName("V2Ray Client");
-  QApplication::setFont(QFont({ "Noto Sans",
-                                "Segoe UI Variable",
-                                "Segoe UI",
-                                "Noto Sans CJK",
-                                "PingFang SC",
-                                "Microsoft YaHei UI" },
-                              10));
+  QApplication::setFont(QFont(fonts, 10));
 
   across::Application app(argc, argv);
 
