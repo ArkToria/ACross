f6c6e7533fbcd02f2f420f8e517c57e541acab49
cocomeow <catnights@pm.me>
2021-10-18 22:16:52 +0800

    add zxing api version check

--- src/models/qrcodetools.h
+++ src/models/qrcodetools.h
@@ -8,6 +8,11 @@
 #include <BarcodeFormat.h>
 #include <BitMatrix.h>
 #include <MultiFormatWriter.h>
+#include <ZXVersion.h>
+
+#define ZXING_VERSION                                                          \
+  QT_VERSION_CHECK(                                                            \
+    ZXING_VERSION_MAJOR, ZXING_VERSION_MINOR, ZXING_VERSION_PATCH)
 
 namespace across {
 namespace utils {
@@ -27,8 +32,12 @@ private:
   uint m_size = 256;
   uint m_margin = 2;
   int m_ecc_level = -1;
+
+#if (ZXING_VERSION > QT_VERSION_CHECK(1, 2, 0))
   ZXing::BarcodeFormat m_format = ZXing::BarcodeFormat::QR_CODE;
-  // QR_CODE is a deprecated name, kept for compatibility at the moment
+#else
+  ZXing::BarcodeFormat m_format = ZXing::BarcodeFormat::QRCode;
+#endif
 };
 }
 }
