beb8dc887524d73a979663ab5fa19c01720a7753
cocomeow <catnights@pm.me>
2021-10-30 23:54:41 +0800

    update log highlight colors

--- src/view_models/loghighlighter.cpp
+++ src/view_models/loghighlighter.cpp
@@ -79,12 +79,12 @@ LogHighlighter::setTheme(const config::Theme& theme)
 
   info_format.setForeground(QColor(colors.text_color().c_str()));
   warning_format.setForeground(QColor(colors.warn_color().c_str()));
-  highlight_format.setForeground(QColor(colors.highlight_color().c_str()));
-  ip_host_format.setForeground(QColor(colors.style_color().c_str()));
-  logger_format.setForeground(QColor(colors.highlight_color().c_str()));
+  highlight_format.setForeground(QColor(colors.style_color().c_str()));
+  ip_host_format.setForeground(QColor(colors.highlight_color().c_str()));
+  logger_format.setForeground(QColor(colors.text_color().c_str()));
   date_format.setForeground(QColor(colors.text_color().c_str()));
   time_format.setForeground(QColor(colors.text_color().c_str()));
-  tcp_udp_format.setForeground(QColor(colors.style_color().c_str()));
+  tcp_udp_format.setForeground(QColor(colors.text_color().c_str()));
   tcp_udp_format.setFontWeight(QFont::Bold);
 }
 
