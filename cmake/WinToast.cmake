368b435819965c8c0924a5da3e74c2c0105d6b9e
霜滅 <kamisatoart@pm.me>
2021-11-06 13:18:41 +0000

    更新 'cmake/WinToast.cmake'

--- cmake/WinToast.cmake
+++ cmake/WinToast.cmake
@@ -3,8 +3,8 @@ option(FETCH_WIN_TOAST "Download WinToast" ON)
 if(FETCH_WIN_TOAST)
     CPMAddPackage(
         NAME WinToast
-        GITHUB_REPOSITORY mohabouje/WinToast
-        GIT_TAG 5e441fd03543b999edb663caf8df7be37c0d575c
+        GITHUB_REPOSITORY moodyhunter/WinToast
+        GIT_TAG cf03283a030d0d043120f4cfed813828cb907899
         )
 else()
     add_subdirectory(${CMAKE_SOURCE_DIR}/3rdpart/WinToast)
