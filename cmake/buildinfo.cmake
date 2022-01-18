2115fa53b4d6582f4c223d6f3bbd3ded9c4c09cd
Moody <mooodyhunter@outlook.com>
2022-01-18 01:57:29 +0000

    fix CMake DATE_TIME_FORMAT variable, optimise functions in buildinfo.h.in

--- cmake/buildinfo.cmake
+++ cmake/buildinfo.cmake
@@ -4,7 +4,7 @@ set(BUILD_INFO "Manually Build" CACHE STRING "Display BuildInfo")
 
 set(FONTS_FALLBACK "Noto Sans, Segoe UI Variable, Segoe UI, Noto Sans CJK, PingFang SC, Microsoft YaHei UI" CACHE STRING "Fonts Fallback")
 
-set(DATE_TIME_FORMAT "MM/dd/yyyy hh:mm:ss")
+set(DATE_TIME_FORMAT "MM/dd/yyyy hh:mm:ss" CACHE STRING "Default date time format")
 
 configure_file(${CMAKE_SOURCE_DIR}/src/view_models/buildinfo.h.in
     ${CMAKE_BINARY_DIR}/generated/buildinfo.h
